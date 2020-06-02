1b4b929c0778ab4779ec6d4e7b8dfe119c60237b
molnarj <molnar.jozsef@avangrade.com>
2020-06-02 20:57:25 +0200

    Collapsible list improved transitions

--- components/CollapsibleListItem.jsx
+++ components/CollapsibleListItem.jsx
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import * as PropTypes from "prop-types";
 import classnames from "classnames";
 
 import "./CollapsibleListItem.scss";
 
 function CollapsibleListItem({ title, content, variant }) {
-  const [open, setOpen] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [transition, setTransition] = React.useState(false);
+  const [contentHeight, setContentHeight] = React.useState(0);
+  const contentRef = useRef();
+
+  const updateHeight = () => {
+    const height = contentRef?.current?.clientHeight || 0;
+    setContentHeight(height && height + 10);
+  };
+
+  const setMaxHeight = () => {
+    const res =
+      !open && transition ? 0 : contentHeight > 0 ? contentHeight + "px" : null;
+    return res;
+  };
+
+  const handleClick = (e) => {
+    if (!transition) {
+      setTransition(true);
+      setOpen(!open);
+      if (open) {
+        setTimeout(() => {
+          setContentHeight(0);
+          setTransition(false);
+        }, 500); // Need to sync with the max-height transition duration value of the .collapsibleListItem__content css class.
+      } else {
+        updateHeight();
+        setTimeout(() => {
+          setTransition(false);
+        }, 100);
+      }
+    }
+  };
+
   return (
     <div className={classnames("collapsibleListItem", variant)}>
-      <div
-        onClick={(e) => {
-          setOpen(!open);
-        }}
-        className="collapsibleListItem__title"
-      >
+      <div onClick={handleClick} className="collapsibleListItem__title">
         <div className={classnames("collapsibleListItem__titleText")}>
           {title}
         </div>
@@ -23,16 +51,26 @@ function CollapsibleListItem({ title, content, variant }) {
               "collapsibleListItem__toggleIcon"
               // "icon-plus-sign" !TODO
             )}
-            style={{ width: "32px", height: "32px", textAlign: 'center' }} // TODO
+            style={{ width: "32px", height: "32px", textAlign: "center" }} // TODO
           >
             {open ? "-" : "+"}
           </i>
         </div>
       </div>
       <div
-        className={classnames("collapsibleListItem__content", open && "-open")}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+        className={classnames(
+          "collapsibleListItem__content",
+          open && "-open",
+          transition && "-transition"
+        )}
+        style={{ maxHeight: setMaxHeight() }}
+        ref={contentRef}
+      >
+        <div
+          className="collapsibleListItem__contentContainer"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
     </div>
   );
 }
