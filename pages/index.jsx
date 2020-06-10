5f1f40da1076155e59fbac2458be6e17db2d9fe0
molnarj <molnar.jozsef@avangrade.com>
2020-06-10 13:35:56 +0200

    Home props refactor

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,84 +6,32 @@ import ActionBlock from "../components/ActionBlock";
 import Article from "../components/Article";
 
 export const getServerSideProps = async () => {
-  const homeContent = await getHomeContent();
+  const content = await getHomeContent();
   return {
     props: {
-      homeContent,
+      content: content,
     },
   };
 };
 
-function Home({ homeContent }) {
+function Home({ content }) {
   const {
-    hero,
     videoBanner,
+
+    hero,
     introduction,
     diagonalBanner,
     article,
     actionBlock,
-  } = homeContent;
+  } = content;
   return (
     <>
-      <MediaBlock
-        title={videoBanner.title}
-        subTitle={videoBanner.subTitle}
-        content={videoBanner.content}
-        asset={{
-          ...videoBanner.asset,
-        }}
-        variant={videoBanner.variant}
-        skiplink
-      />
-      <MediaBlock
-        title={hero.title}
-        subTitle={hero.subTitle}
-        content={hero.content}
-        asset={{
-          ...hero.asset,
-        }}
-        video={{
-          type: hero.video.type,
-          videoId: hero.video.videoId,
-          buttonLabel: hero.video.buttonLabel,
-        }}
-        variant={[...hero.variant]}
-      />
-      <MediaBlock
-        title={introduction.title}
-        subTitle={introduction.subTitle}
-        asset={{
-          ...introduction.asset,
-        }}
-        icon={introduction.icon}
-        variant={introduction.variant}
-      />
-      <Article
-        title={article.title}
-        subTitle={article.subTitle}
-        video={article.video}
-        icon={article.icon}
-        variant={article.variant}
-        ctaButton={article.ctaButton}
-        content={article.content}
-        image={article.image}
-      />
-      <MediaBlock
-        title={diagonalBanner.title}
-        subTitle={diagonalBanner.subTitle}
-        asset={{
-          ...diagonalBanner.asset,
-        }}
-        ctaButton={{
-          ...diagonalBanner.ctaButton,
-        }}
-        variant={diagonalBanner.variant}
-      />
-      <ActionBlock
-        title={actionBlock.title}
-        subTitle={actionBlock.subTitle}
-        video={actionBlock.video}
-      />
+      <MediaBlock {...videoBanner} skiplink />
+      <MediaBlock {...hero} />
+      <MediaBlock {...introduction} />
+      <Article {...article} />
+      <MediaBlock {...diagonalBanner} />
+      <ActionBlock {...actionBlock} />
     </>
   );
 }
